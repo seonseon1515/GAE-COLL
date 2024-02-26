9f712a3ed5fade4ea5267aad4e6b7040f64b8358
12jason0 <12jason0@spreatics.com>
2024-02-26 20:26:03 +0900

    start

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 app.set('view engine', 'ejs');
+app.use('/public', express.static(__dirname + '/public'));
 
 const pageRouter = require('./routes/page');
 app.use('/', pageRouter);
---
+++ public/js/start/kakao.js
@@ -0,0 +1,7 @@
+<script
+    src="https://t1.kakaocdn.net/kakao_js_sdk/${VERSION}/kakao.min.js"
+    integrity="${INTEGRITY_VALUE}"
+    crossorigin="anonymous"
+></script>;
+Kakao.init('dee402ec53e7dea3b67fb76057f12b34');
+Kakao.isInitialized();
