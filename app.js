b859df90c1c35dfafeb3a7c04e7017354550e209
12jason0 <12jason0@spreatics.com>
2024-02-26 15:31:23 +0900

    devel_start

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('start/index');
+});
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
