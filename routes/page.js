da5a64d6c3b92e6372b9abcbaa56db0e1d39b601
12jason0 <12jason0@spreatics.com>
2024-02-28 09:22:49 +0900

    header, start 수정

--- routes/page.js
+++ routes/page.js
@@ -13,4 +13,5 @@ router.get('/header', controller.header);
 router.get('/header/project', controller.projectheader);
 router.get('/project', controller.project);
 router.get('/mypage', controller.mypage);
+router.get('/footer', controller.footer);
 module.exports = router;
