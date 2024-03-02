dc21d38d1b4e337a40ded822958690a6daf6af9e
Hyunkyung-Nam <gusrud13579@gmail.com>
2024-03-02 18:27:24 +0900

    이미지 업로드시 해당하는 확장자만 올라가도록 구현, 프로젝트 생성시 이미지 확장자가 틀린경우 생성 불가하도록 구현, 프로젝트 이미지 및 파일 삭제시 기존 파일 삭제, 파일 수정 기능 추가

--- routes/project.js
+++ routes/project.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const controller = require("../controller/project");
 const router = express.Router();
-const { upload } = require("../middleware/multer");
+const { uploadProjectImg, uploadProjectFiles } = require("../middleware/multer");
 const { auth } = require("../middleware/auth");
 
 //프로젝트 생성
-router.post("/create", auth, upload.single("project_img"), controller.createProject);
+router.post("/create", auth, uploadProjectImg, controller.createProject);
 //내 작업 조회
 router.post("/board/mine", auth, controller.getMyBoard);
 //내 프로젝트 조회
@@ -20,7 +20,7 @@ router.post("/get/log", auth, controller.projectLog);
 //프로젝트 이름 수정
 router.patch("/update/name", auth, controller.updateProjectName);
 //프로젝트 이미지 수정
-router.patch("/update/img", auth, upload.single("project_img"), controller.updateProjectImg);
+router.patch("/update/img", auth, uploadProjectImg, controller.updateProjectImg);
 //프로젝트 상태 수정
 router.patch("/update/status", auth, controller.updateProjectStatus);
 //프로젝트 기간 수정
@@ -34,7 +34,9 @@ router.post("/add/member", auth, controller.addProjectMember);
 //프로젝트 멤버 삭제
 router.delete("/delete/member", auth, controller.deleteProjectMember);
 //프로젝트 파일 수정
-//router.patch('/update/file', auth, uploadDetail.array('files'), controller.updateProjectFile);
+router.patch("/update/file", auth, uploadProjectFiles, controller.updateProjectFile);
+//프로젝트 파일 삭제
+router.delete("/delete/file", auth, controller.deleteFile);
 //프로젝트 깃허브 수정
 router.patch("/update/github", auth, controller.updateProjectGithub);
 
