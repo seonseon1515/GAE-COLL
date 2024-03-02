a6bf792d5ba8d172da9b8cdb466852051210cffa
Hyunkyung-Nam <gusrud13579@gmail.com>
2024-03-02 19:45:01 +0900

    board : 보드작성, 보드전체조회, 보드 1개 조회, 보드 월별 조회, 보드 수정, 보드 삭제, 보드에 해당하는 댓글 조회, 댓글작성, 댓글삭제

--- controller/project_board.js
+++ controller/project_board.js
@@ -1,7 +1,28 @@
-const { Board } = require("../models");
+const { Board, BoardComment } = require("../models");
+
+//보드 전체 조회
+exports.getBoardAll = async (req, res) => {
+    try {
+        const { project_id } = req.params;
+
+        const getBoardAllResult = await Board.findAll({
+            order: [["id", "DESC"]],
+            where: { project_id },
+        });
+        res.json({ success: true, result: getBoardAllResult.data });
+    } catch (error) {
+        res.json({ success: false, result: error });
+    }
+};
+
+//보드작성
 exports.boardWrite = async (req, res) => {
+    my_id = req.userId;
     try {
         const { project_id, board_title, board_description, user_id, status, deadline } = req.body;
+        let is_mine = false;
+
+        user_id.include(is_mine) ? (is_mine = true) : (is_mine = false);
 
         const boardWriteResult = await Board.create({
             project_id,
@@ -10,13 +31,99 @@ exports.boardWrite = async (req, res) => {
             user_id,
             status,
             deadline,
+            is_mine,
         });
         res.json({ success: true, result });
     } catch (error) {
         res.json({ success: false, result: error });
     }
 };
+//보드 1개조회
 exports.getBoardDetail = async (req, res) => {
-    //보드 1개 조회시 보드 코멘트도 조회하기
+    try {
+        const { project_id, board_id: id } = req.params;
+        const getBoardDatail = await Board.findOne({
+            order: [["id", "DESC"]],
+            where: { id, project_id },
+        });
+        res.json({ success: true, result: getBoardDatail.data });
+    } catch (error) {
+        res.json({ success: false, result: error });
+    }
 };
-exports.getBoardMonth = async (req, res) => {};
+//보드 월별조회
+exports.getBoardMonth = async (req, res) => {
+    try {
+        const { project_id, YYYYMM } = req.params;
+
+        const getBoardAllResult = await Board.findAll({
+            order: [["id", "DESC"]],
+            where: { project_id, deadline: { [Op.like]: `${YYYYMM}%` } },
+        });
+        res.json({ success: true, result: getBoardAllResult.data });
+    } catch (error) {
+        res.json({ success: false, result: error });
+    }
+};
+//보드 수정
+exports.updateBoard = async (req, res) => {};
+//보드 삭제
+exports.deleteBoard = async (req, res) => {
+    try {
+        my_id = req.userId;
+        const { board_id: boardId } = req.body;
+        const deleteBoardResult = await Board.delete({
+            where: {
+                boardId,
+            },
+        });
+        res.json({ success: true, result: "" });
+    } catch (error) {
+        res.json({ success: false, result: error });
+    }
+};
+//보드에 해당하는 댓글 가져오기
+exports.getComment = async (req, res) => {
+    try {
+        const { project_id: projectId, board_id: id } = req.params;
+
+        const getCommentAllResult = await BoardComment.findAll({
+            order: [["id", "DESC"]],
+            where: { projectId, id },
+        });
+        res.json({ success: true, result: getCommentAllResult.data });
+    } catch (error) {
+        res.json({ success: false, result: error });
+    }
+};
+//댓글 작성
+exports.writeCommnet = async (req, res) => {
+    try {
+        my_id = req.userId;
+        const { project_id: projectId, comment, board_id: boardId } = req.body;
+        const createCommentResult = await BoardComment.create({
+            projectId,
+            boardId,
+            comment,
+            user_id: my_id,
+        });
+        res.json({ success: true, result: "" });
+    } catch (error) {
+        res.json({ success: false, result: error });
+    }
+};
+//댓글 삭제
+exports.deleteComment = async (req, res) => {
+    try {
+        my_id = req.userId;
+        const { board_id: boardId } = req.body;
+        const deleteCommentResult = await BoardComment.delete({
+            where: {
+                boardId,
+            },
+        });
+        res.json({ success: true, result: "" });
+    } catch (error) {
+        res.json({ success: false, result: error });
+    }
+};
--- routes/project_board.js
+++ routes/project_board.js
@@ -4,12 +4,22 @@ const router = express.Router();
 const { auth } = require("../middleware/auth");
 
 //보드 조회
-router.post("/board/get", auth);
-//보드 작성
-router.post("/write", auth);
-//보드 1개 조회
-router.post("/detail", auth);
+router.get("/get", auth, controller.getBoardAll);
 //보드 월별 조회
-router.post("/month/get", auth);
+router.get("/get/month", auth, controller.getBoardMonth);
+//보드 수정
+router.patch("/update", auth, controller.updateBoard);
+//보드 삭제
+router.delete("/delete", auth, controller.deleteBoard);
+//보드 작성
+router.post("/write", auth, controller.boardWrite);
+//보드 1개 조회
+router.get("/detail", auth, controller.getBoardDetail);
+//보드 댓글 조회
+router.get("/get/comment", auth, controller.getComment);
+//보드 댓글 작성
+router.post("/write/comment", auth, controller.writeCommnet);
+//보드 댓글 삭제
+router.delete("/delete/comment", auth, controller.deleteComment);
 
 module.exports = router;
