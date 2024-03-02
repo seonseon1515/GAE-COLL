e76912b43b19499d6b573c59e3bf8493048fcdd3
ahyoung <doubletwosep@gmail.com>
2024-03-02 20:03:07 +0900

    pull받기 위한 커밋

--- controller/project_issue.js
+++ controller/project_issue.js
@@ -7,16 +7,19 @@ const { Op } = require("sequelize");
 exports.getProjectIssues = async (req, res) => {
     try {
         const { project_id: projectId } = req.body; // 프로젝트 ID
-        const user_id = req.userId; // 작성자 ID
-        console.log(project_id, user_id);
-        // 프로젝트 멤버 여부 확인?
-        // where user_id is project_id === projectId면 프로젝트 멤버
-
-        const projectIssues = await Issue.findAll({ where: { projectId } });
-        res.json({ success: true, projectIssues });
+        const userId = req.userId; // 작성자 ID
+        console.log(projectId, userId);
+        const isMember = await ProjectMember.findOne({ where: { userId, projectId } });
+        // 프로젝트 멤버 여부 확인
+        if (isMember) {
+            const projectIssues = await Issue.findAll({ where: { projectId } });
+            res.json({ success: true, result: projectIssues });
+        } else {
+            res.json({ success: false, result: "프로젝트 멤버만 접근할 수 있습니다." });
+        }
     } catch (error) {
         console.error("프로젝트 이슈 조회 오류:", error);
-        res.status(500).json({ success: false });
+        res.json({ success: false, result: error });
     }
 };
 
@@ -25,11 +28,15 @@ exports.searchProjectIssues = async (req, res) => {
     const { keyword } = req.query;
 
     try {
-        const projectIssues = await Issue.findAll({ where: { title: { [Op.like]: `%${keyword}%` } } });
-        res.json({ success: true, projectIssues });
+        if (isMemeber) {
+            const projectIssues = await Issue.findAll({ where: { title: { [Op.like]: `%${keyword}%` } } });
+            res.json({ success: true, result: projectIssues });
+        } else {
+            res.json({ success: true, result: "검색 권한이 없습니다." });
+        }
     } catch (error) {
         console.error("프로젝트 이슈 검색 오류:", error);
-        res.status(500).json({ success: false, message: "서버 오류로 프로젝트 이슈 검색에 실패했습니다." });
+        res.json({ success: false, result: error });
     }
 };
 
@@ -38,37 +45,39 @@ exports.getProjectIssueDetail = async (req, res) => {
     const { id, project_id } = req.params;
 
     try {
-        const projectIssue = await Issue.findByPk(id);
-        if (projectIssue) {
-            console.log(projectIssue);
-            res.json({ success: true, result: projectIssue });
+        if (isMemeber) {
+            const projectIssue = await Issue.findByPk(id);
+            if (projectIssue) {
+                console.log(projectIssue);
+                res.json({ success: true, result: projectIssue });
+            } else {
+                res.json({ success: false, result: "해당 프로젝트 이슈를 찾을 수 없습니다." });
+            }
         } else {
-            res.status(404).json({ success: false, result: "해당 프로젝트 이슈를 찾을 수 없습니다." });
+            res.json({ success: false, result: "상제 조회 권한이 없습니다." });
         }
     } catch (error) {
         console.error("프로젝트 이슈 상세 조회 오류:", error);
-        res.status(500).json({ success: false });
+        res.json({ success: false, result: error });
     }
 };
 
 // 프로젝트 이슈 작성
 exports.createProjectIssue = async (req, res) => {
-    const { issue_title, issue_content, project_id, files } = req.body;
+    const { issue_title, issue_content, project_id: projectId, files } = req.body;
+    const userId = req.userId;
     try {
-        const userId = req.userId;
-
         // 프로젝트 멤버 여부 확인
-        const member = await isMember(project_id, userId);
-        if (member) {
+        if (isMember) {
             // 현재 사용자가 프로젝트의 멤버일 경우에만 이슈 작성
-            const newProjectIssue = await Issue.create({ issue_title, issue_content, project_id });
-            res.json({ success: true, newProjectIssue });
+            const newProjectIssue = await Issue.create({ issue_title, issue_content, projectId });
+            res.json({ success: true, result: newProjectIssue });
         } else {
             // 현재 사용자가 프로젝트의 멤버가 아닌 경우 에러 반환
-            res.status(403).json({ success: false, message: "해당 프로젝트의 멤버가 아닙니다." });
+            res.json({ success: false, result: "프로젝트 멤버만 작성할 수 있습니다" });
         }
     } catch (error) {
         console.error("프로젝트 이슈 작성 오류:", error);
-        res.status(500).json({ success: false, message: "서버 오류로 인해 프로젝트 이슈 작성에 실패했습니다." });
+        res.json({ success: false, result: error });
     }
 };
--- models/index.js
+++ models/index.js
@@ -1,65 +1,65 @@
-'use strict';
+"use strict";
 
-const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 let sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 //db table생성
-db.User = require('./user')(sequelize);
-db.Project = require('./project')(sequelize);
-db.ProjectMember = require('./project_member')(sequelize);
-db.ProjectFile = require('./project_file')(sequelize);
-db.Board = require('./board')(sequelize);
-db.BoardComment = require('./board_comment')(sequelize);
-db.Issue = require('./issue')(sequelize);
-db.IssueComment = require('./issue_comment')(sequelize);
+db.User = require("./user")(sequelize);
+db.Project = require("./project")(sequelize);
+db.ProjectMember = require("./project_member")(sequelize);
+db.ProjectFile = require("./project_file")(sequelize);
+db.Board = require("./board")(sequelize);
+db.BoardComment = require("./board_comment")(sequelize);
+db.Issue = require("./issue")(sequelize);
+db.IssueComment = require("./issue_comment")(sequelize);
 
 //ProjectMember 외래키 설정 userId
-db.User.hasMany(db.ProjectMember, { foreignKey: 'userId', onDelete: 'CASCADE' });
-db.ProjectMember.belongsTo(db.User, { foreignKey: 'userId', onDelete: 'CASCADE' });
+db.User.hasMany(db.ProjectMember, { foreignKey: "userId", onDelete: "CASCADE" });
+db.ProjectMember.belongsTo(db.User, { foreignKey: "userId", onDelete: "CASCADE" });
 
 //ProjectMember 외래키 설정 projectId
-db.Project.hasMany(db.ProjectMember, { foreignKey: 'projectId', onDelete: 'CASCADE' });
-db.ProjectMember.belongsTo(db.Project, { foreignKey: 'projectId', onDelete: 'CASCADE' });
+db.Project.hasMany(db.ProjectMember, { foreignKey: "projectId", onDelete: "CASCADE" });
+db.ProjectMember.belongsTo(db.Project, { foreignKey: "projectId", onDelete: "CASCADE" });
 
 //ProjectFile 외래키 설정 projectId
-db.Project.hasMany(db.ProjectFile, { foreignKey: 'projectId', onDelete: 'CASCADE' });
-db.ProjectFile.belongsTo(db.Project, { foreignKey: 'projectId', onDelete: 'CASCADE' });
+db.Project.hasMany(db.ProjectFile, { foreignKey: "projectId", onDelete: "CASCADE" });
+db.ProjectFile.belongsTo(db.Project, { foreignKey: "projectId", onDelete: "CASCADE" });
 
 //Board 외래키 설정 projectId
-db.Project.hasMany(db.Board, { foreignKey: 'projectId', onDelete: 'CASCADE' });
-db.Board.belongsTo(db.Project, { foreignKey: 'projectId', onDelete: 'CASCADE' });
+db.Project.hasMany(db.Board, { foreignKey: "projectId", onDelete: "CASCADE" });
+db.Board.belongsTo(db.Project, { foreignKey: "projectId", onDelete: "CASCADE" });
 
 //Board 외래키 설정 userId
-db.User.hasMany(db.Board, { foreignKey: 'userId', onDelete: 'CASCADE' });
-db.Board.belongsTo(db.User, { foreignKey: 'userId', onDelete: 'CASCADE' });
+db.User.hasMany(db.Board, { foreignKey: "userId", onDelete: "CASCADE" });
+db.Board.belongsTo(db.User, { foreignKey: "userId", onDelete: "CASCADE" });
 
 //BoardComment 외래키 설정 boardId
-db.Board.hasMany(db.BoardComment, { foreignKey: 'boardId', onDelete: 'CASCADE' });
-db.BoardComment.belongsTo(db.Board, { foreignKey: 'boardId', onDelete: 'CASCADE' });
+db.Board.hasMany(db.BoardComment, { foreignKey: "boardId", onDelete: "CASCADE" });
+db.BoardComment.belongsTo(db.Board, { foreignKey: "boardId", onDelete: "CASCADE" });
 
 //BoardComment 외래키 설정 userId
-db.User.hasMany(db.BoardComment, { foreignKey: 'userId', onDelete: 'CASCADE' });
-db.BoardComment.belongsTo(db.User, { foreignKey: 'userId', onDelete: 'CASCADE' });
+db.User.hasMany(db.BoardComment, { foreignKey: "userId", onDelete: "CASCADE" });
+db.BoardComment.belongsTo(db.User, { foreignKey: "userId", onDelete: "CASCADE" });
 
 //Issue 외래키 설정projectId
-db.Project.hasMany(db.Issue, { foreignKey: 'projectId', onDelete: 'CASCADE' });
-db.Issue.belongsTo(db.Project, { foreignKey: 'projectId', onDelete: 'CASCADE' });
+db.Project.hasMany(db.Issue, { foreignKey: "projectId", onDelete: "CASCADE" });
+db.Issue.belongsTo(db.Project, { foreignKey: "projectId", onDelete: "CASCADE" });
 
 //Issue 외래키 설정 userId
-db.User.hasMany(db.Issue, { foreignKey: 'userId', onDelete: 'CASCADE' });
-db.Issue.belongsTo(db.User, { foreignKey: 'userId', onDelete: 'CASCADE' });
+db.User.hasMany(db.Issue, { foreignKey: "userId", onDelete: "CASCADE" });
+db.Issue.belongsTo(db.User, { foreignKey: "userId", onDelete: "CASCADE" });
 
 //IssueComment 외래키 설정 userId
-db.User.hasMany(db.IssueComment, { foreignKey: 'userId', onDelete: 'CASCADE' });
-db.IssueComment.belongsTo(db.User, { foreignKey: 'userId', onDelete: 'CASCADE' });
+db.User.hasMany(db.IssueComment, { foreignKey: "userId", onDelete: "CASCADE" });
+db.IssueComment.belongsTo(db.User, { foreignKey: "userId", onDelete: "CASCADE" });
 
 //IssueComment 외래키 설정 issueId
-db.Issue.hasMany(db.IssueComment, { foreignKey: 'issueId', onDelete: 'CASCADE' });
-db.IssueComment.belongsTo(db.Issue, { foreignKey: 'issueId', onDelete: 'CASCADE' });
+db.Issue.hasMany(db.IssueComment, { foreignKey: "issueId", onDelete: "CASCADE" });
+db.IssueComment.belongsTo(db.Issue, { foreignKey: "issueId", onDelete: "CASCADE" });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
