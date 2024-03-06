c82ab785c71f092a4b65cd3358b30e17cb0aa47e
12jason0 <12jason0@spreatics.com>
2024-03-06 13:09:08 +0900

    mypage js

--- controller/user.js
+++ controller/user.js
@@ -1,8 +1,8 @@
-const { User } = require("../models");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const { smtpTransport } = require("../config/email");
-var fs = require("fs");
+const { User } = require('../models');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const { smtpTransport } = require('../config/email');
+var fs = require('fs');
 
 //회원가입
 //이메일, 카카오, 구글은 type으로 구분
@@ -10,7 +10,7 @@ exports.signup = async (req, res) => {
     const { email, type, user_name, password: pw, profile_img, thumb_img, selected_question, answer } = req.body;
 
     try {
-        if (type === "email") {
+        if (type === 'email') {
             //패스워드 암호화
             const password = await bcrypt.hash(String(pw), 11);
 
@@ -24,14 +24,14 @@ exports.signup = async (req, res) => {
             });
             console.log(result);
             res.json(result);
-        } else if (type === "kakao" || type === "google") {
+        } else if (type === 'kakao' || type === 'google') {
             const result = await User.create({
                 email,
                 type,
                 user_name,
                 user_img: profile_img,
             });
-            const token = jwt.sign({ id: result.id }, process.env.DEVEL_SECRET, { expiresIn: "1h" });
+            const token = jwt.sign({ id: result.id }, process.env.DEVEL_SECRET, { expiresIn: '24h' });
             res.json({ success: true, result, token });
         }
     } catch (error) {
@@ -47,7 +47,7 @@ exports.emailAuth = async (req, res) => {
     const mailOptions = {
         from: process.env.SMTP_ID, // 발신자 이메일 주소.
         to: email, //사용자가 입력한 이메일 -> 목적지 주소 이메일
-        subject: " GAE-COLL 인증 관련 메일 입니다. ",
+        subject: ' GAE-COLL 인증 관련 메일 입니다. ',
         html: `<h3 style="padding-top:10px;">GAE-COLL 이메일 인증</h3>
         <div>안녕하세요 GAE-COLL 회원가입을 위한 이메일 인증번호를 안내해드립니다.</div> 
         <div>아래 번호를 입력하여 인증 절차를 완료해 주세요.</div>
@@ -55,14 +55,14 @@ exports.emailAuth = async (req, res) => {
         <img src='https://kdt11-hk-test.s3.ap-northeast-2.amazonaws.com/logo.png' width="300px"/>`,
     };
     smtpTransport.sendMail(mailOptions, (err, response) => {
-        console.log("response", response);
+        console.log('response', response);
         //첫번째 인자는 위에서 설정한 mailOption을 넣어주고 두번째 인자로는 콜백함수.
         if (err) {
-            res.json({ success: false, result: { message: " 메일 전송에 실패하였습니다. " } });
+            res.json({ success: false, result: { message: ' 메일 전송에 실패하였습니다. ' } });
             smtpTransport.close(); //전송종료
             return;
         } else {
-            res.json({ success: true, result: { message: " 메일 전송에 성공하였습니다. ", authNum: number } });
+            res.json({ success: true, result: { message: ' 메일 전송에 성공하였습니다. ', authNum: number } });
             smtpTransport.close(); //전송종료
             return;
         }
@@ -82,7 +82,7 @@ exports.emailAuth = async (req, res) => {
         //비밀번호 일치시
         if (password) {
             //jwt토큰 발행
-            const token = jwt.sign({ id: loginResult.id }, process.env.DEVEL_SECRET, { expiresIn: "1h" });
+            const token = jwt.sign({ id: loginResult.id }, process.env.DEVEL_SECRET, { expiresIn: '24h' });
             res.json({ success: true, token });
         } else {
             res.json({ success: false, message: error });
@@ -95,7 +95,7 @@ exports.emailAuth = async (req, res) => {
 //유저이메일을 이용한 유저조회
 exports.findUser = async (req, res) => {
     const { email, isSignup } = req.body;
-    console.log("email", email, "isSignup", isSignup);
+    console.log('email', email, 'isSignup', isSignup);
     try {
         const findUserResult = await User.findOne({
             where: { email },
@@ -106,7 +106,7 @@ exports.findUser = async (req, res) => {
 
         if (findUserResult) {
             if (isSignup) {
-                const token = jwt.sign({ id: findUserResult.id }, process.env.DEVEL_SECRET, { expiresIn: "1h" });
+                const token = jwt.sign({ id: findUserResult.id }, process.env.DEVEL_SECRET, { expiresIn: '24h' });
                 res.json({ success: true, token });
             } else {
                 res.json({ success: true, findUserResult });
@@ -123,7 +123,7 @@ exports.findUser = async (req, res) => {
 //유저 프로필조회
 exports.getUserInfo = async (req, res) => {
     const userId = req.userId;
-    console.log("유저프로필 조회", userId);
+    console.log('유저프로필 조회', userId);
     try {
         const getUserInfoRes = await User.findOne({ where: { id: Number(userId) } });
         res.json({ success: true, result: getUserInfoRes });
@@ -137,7 +137,7 @@ exports.updateUser = async (req, res) => {
     const userId = req.userId;
     // console.log("여러개", userId);
     const { user_name } = req.body;
-    console.log("유저네임 객체 ", user_name);
+    console.log('유저네임 객체 ', user_name);
     try {
         const updatedResult = await User.update({ user_name }, { where: { id: userId } });
         console.log(updatedResult);
@@ -151,12 +151,39 @@ exports.updateUser = async (req, res) => {
         res.json({ success: false, result: error });
     }
 };
+//비밀번호 확인
+exports.checkPW = async (req, res) => {
+    const userId = req.userId;
 
+    const { password } = req.body;
+
+    try {
+        const hashedNewPassword = await bcrypt.hash(password, 11);
+        const checkPWResult = await User.findOne({ password: hashedNewPassword }, { where: { id: userId } });
+        if (checkPWResult) {
+            res.json({
+                success: true,
+                result: {
+                    message: '비밀번호 맞음',
+                },
+            });
+        } else {
+            res.json({
+                success: false,
+                result: {
+                    message: '비밀번호 틀림',
+                },
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, result: error });
+    }
+};
 // 유저 비밀번호 수정
 exports.updatePassword = async (req, res) => {
     const userId = req.userId;
     const { password } = req.body;
-
     try {
         // 새로운 비밀번호 해싱
         const hashedNewPassword = await bcrypt.hash(password, 11);
@@ -175,7 +202,7 @@ exports.updateUserImage = async (req, res) => {
     const file = req.file;
 
     if (file === undefined) {
-        res.json({ success: false, result: { message: "파일업로드에 실패하였습니다." } });
+        res.json({ success: false, result: { message: '파일업로드에 실패하였습니다.' } });
         return;
     }
     deleteImg(id);
@@ -200,14 +227,14 @@ exports.findID = async (req, res) => {
         const findIDresult = await User.findOne({
             where: { user_name, selected_question: Number(selected_question), answer },
         });
-        console.log("findIDresult", findIDresult);
+        console.log('findIDresult', findIDresult);
         if (findIDresult) {
             res.json({ success: true, result: findIDresult.email });
         } else {
-            res.json({ success: false, result: { message: "해당정보를 가진 유저가 없습니다." } });
+            res.json({ success: false, result: { message: '해당정보를 가진 유저가 없습니다.' } });
         }
     } catch (error) {
-        console.log("아이디 찾기 오류", error);
+        console.log('아이디 찾기 오류', error);
         res.json({ success: false, result: error });
     }
 };
@@ -252,9 +279,9 @@ async function deleteImg(id) {
         const getProfileInfotResult = await User.findOne({
             where: { id },
         });
-        if (fs.existsSync("./public/uploads/profile/" + getProfileInfotResult.user_img)) {
+        if (fs.existsSync('./public/uploads/profile/' + getProfileInfotResult.user_img)) {
             // 파일이 존재한다면 true 그렇지 않은 경우 false 반환
-            fs.unlinkSync("./public/uploads/profile/" + getProfileInfotResult.user_img);
+            fs.unlinkSync('./public/uploads/profile/' + getProfileInfotResult.user_img);
         }
     } catch (error) {
         console.log(error);
