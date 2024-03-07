054c4dca04cb8f90504a8ae6a1aae9166611440c
12jason0 <12jason0@spreatics.com>
2024-03-07 10:57:33 +0900

    2020

--- public/js/project/home.js
+++ public/js/project/home.js
@@ -1,6 +1,66 @@
+(async function loadProjectInfo() {
+    const token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNzA5Nzc1MjgxLCJleHAiOjE3MDk4NjE2ODF9.CT85wTPL9HD_ofa0lV8cCU6zegiK0SKddI9DiAjjuUQ";
+    const project_id = 11;
+
+    try {
+        const response = await axios({
+            method: "POST",
+            url: `/api/project/get/info`,
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            data: {
+                project_id,
+            },
+        });
+        console.log(response);
+        const { success, result } = response.data;
+        if (success) {
+            document.getElementById("overview-main").textContent = result.overview;
+            //document.getElementById("member-main").textContent =;
+            const rule = JSON.parse(result.rule);
+
+            for (let i = 0; i < rule.length; i++) {
+                const html = `
+                <li class="li">
+                    <span class="text-element">${rule[i]}</span>
+                    <span class="rule-icon">
+                    <img src="../../public/img/edit-right.jpg" class="rule-img edit-icon" />
+                    <img src="../../public/img/trash.png" class="rule-img delete-icon" />
+                </span>
+                </li>
+                `;
+                document.getElementById("rule-list").insertAdjacentHTML("afterend", html);
+            }
+        } else {
+            console.error("API 요청 실패:", response.data);
+        }
+    } catch (error) {
+        console.error("API 요청 중 오류 발생:", error);
+    }
+})();
+
+(async function () {
+    const getProjectFile = await axios({
+        method: "POST",
+        url: "/api/project/get/file",
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+        data: {
+            project_id,
+        },
+    });
+    const {
+        success,
+        result: {
+            file: {},
+        },
+    } = getProjectFile.data;
+});
 //project overview 수정
-let token = localStorage.getItem("token");
-let projectId = localStorage.getItem("project_id");
+// let projectId = localStorage.getItem("project_id");
 async function changeOverview(e) {
     const text = document.getElementById("overview-text");
     const input = document.getElementById("overview-input");
@@ -8,6 +68,9 @@ async function changeOverview(e) {
     input.value = text.textContent;
     text.style.display = "none";
     input.focus();
+    const token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNzA5Nzc1MjgxLCJleHAiOjE3MDk4NjE2ODF9.CT85wTPL9HD_ofa0lV8cCU6zegiK0SKddI9DiAjjuUQ";
+    const project_id = 1;
 }
 
 async function saveOverview(event) {
@@ -26,7 +89,7 @@ async function saveOverview(event) {
                 Authorization: `Bearer ${token}`,
             },
             data: {
-                project_id: projectId,
+                project_id,
                 overview: input.value,
             },
         });
@@ -42,220 +105,220 @@ async function saveOverview(event) {
     }
 }
 
-//member 추가
+// //member 추가
 
-function changeOverview() {
-    const userId = prompt("추가할 멤버의 아이디를 입력하세요.");
-    if (userId) {
-        // 입력한 아이디를 localStorage에 저장
-        localStorage.setItem("userId", userId);
-    }
-}
+// function addMember() {
+//     const userId = prompt("추가할 멤버의 아이디를 입력하세요.");
+//     if (userId) {
+//         // 입력한 아이디를 localStorage에 저장
+//         localStorage.setItem("userId", userId);
+//     }
+// }
 
-async function addMember() {
-    let projectId = "REAL_PROJECT_ID"; // 실제 프로젝트 ID로 변경
-    let token = localStorage.getItem("token");
-    let memberIds = [];
+// async function addMember() {
+//     let projectId = "REAL_PROJECT_ID"; // 실제 프로젝트 ID로 변경
+//     let token = localStorage.getItem("token");
+//     let memberIds = [];
 
-    // localStorage에서 아이디를 가져옴
-    const userId = localStorage.getItem("userId");
-    if (userId) {
-        // 사용자 아이디가 존재하는지 확인
-        const user = await checkUserId(userId);
-        if (user) {
-            memberIds.push(user.id); // 데이터베이스에 존재하는 사용자의 아이디를 memberIds 배열에 추가
+//     // localStorage에서 아이디를 가져옴
+//     const userId = localStorage.getItem("userId");
+//     if (userId) {
+//         // 사용자 아이디가 존재하는지 확인
+//         const user = await checkUserId(userId);
+//         if (user) {
+//             memberIds.push(user.id); // 데이터베이스에 존재하는 사용자의 아이디를 memberIds 배열에 추가
 
-            // 사용자 이름을 화면에 추가
-            const userNameDiv = document.getElementById("overview-main");
-            userNameDiv.innerHTML = `
-                <div id="pro-img-div">
-                    <img src="../../public/img/mypage.png" id="pen-img" />
-                </div>
-                ${user.name} // user 객체에서 사용자 이름을 가져옴
-            `;
-        } else {
-            console.log(`아이디 ${userId}은 존재하지 않습니다.`);
-            return;
-        }
-    } else {
-        console.log("유효한 아이디를 입력하세요");
-        alert("유효한 아이디를 입력하세요");
-        return;
-    }
+//             // 사용자 이름을 화면에 추가
+//             const userNameDiv = document.getElementById("overview-main");
+//             userNameDiv.innerHTML = `
+//                 <div id="pro-img-div">
+//                     <img src="../../public/img/mypage.png" id="pen-img" />
+//                 </div>
+//                 ${user.name} // user 객체에서 사용자 이름을 가져옴
+//             `;
+//         } else {
+//             console.log(`아이디 ${userId}은 존재하지 않습니다.`);
+//             return;
+//         }
+//     } else {
+//         console.log("유효한 아이디를 입력하세요");
+//         alert("유효한 아이디를 입력하세요");
+//         return;
+//     }
 
-    try {
-        const response = await axios({
-            method: "POST",
-            url: "/api/project/add/member",
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-            data: {
-                project_id: projectId,
-                member_id: memberIds[0], // 첫 번째 멤버의 ID만 전송
-            },
-        });
-        console.log(response);
-        const { success, result } = response.data;
-        if (success) {
-            console.log("회원 추가 성공", result);
-        } else {
-            console.log(response);
-            console.log("회원 추가 실패");
-        }
-    } catch (error) {
-        console.error("회원 추가 중 에러 발생", error);
-    }
-}
+//     try {
+//         const response = await axios({
+//             method: "POST",
+//             url: "/api/project/add/member",
+//             headers: {
+//                 Authorization: `Bearer ${token}`,
+//             },
+//             data: {
+//                 project_id: projectId,
+//                 member_id: memberIds[0], // 첫 번째 멤버의 ID만 전송
+//             },
+//         });
+//         console.log(response);
+//         const { success, result } = response.data;
+//         if (success) {
+//             console.log("회원 추가 성공", result);
+//         } else {
+//             console.log(response);
+//             console.log("회원 추가 실패");
+//         }
+//     } catch (error) {
+//         console.error("회원 추가 중 에러 발생", error);
+//     }
+// }
 
-//project 규칙 추가
-function attachEventHandlers(li) {
-    const textElement = li.querySelector(".text-element");
-    const input = li.querySelector("input");
+// //project 규칙 추가
+// function attachEventHandlers(li) {
+//     const textElement = li.querySelector(".text-element");
+//     const input = li.querySelector("input");
 
-    input.onkeydown = function (event) {
-        if (event.key !== "Enter") return;
-        textElement.textContent = input.value;
-        input.style.display = "none";
-        textElement.style.display = "";
-    };
+//     input.onkeydown = function (event) {
+//         if (event.key !== "Enter") return;
+//         textElement.textContent = input.value;
+//         input.style.display = "none";
+//         textElement.style.display = "";
+//     };
 
-    const editIcon = li.querySelector(".edit-icon");
-    editIcon.onclick = function () {
-        input.value = textElement.textContent;
-        input.style.display = "";
-        textElement.style.display = "none";
-        input.focus();
-    };
+//     const editIcon = li.querySelector(".edit-icon");
+//     editIcon.onclick = function () {
+//         input.value = textElement.textContent;
+//         input.style.display = "";
+//         textElement.style.display = "none";
+//         input.focus();
+//     };
 
-    const deleteIcon = li.querySelector(".delete-icon");
-    deleteIcon.onclick = function () {
-        li.parentNode.removeChild(li);
-    };
-}
+//     const deleteIcon = li.querySelector(".delete-icon");
+//     deleteIcon.onclick = function () {
+//         li.parentNode.removeChild(li);
+//     };
+// }
 
-window.onload = function () {
-    const lis = document.querySelectorAll("#rule-list .li");
-    lis.forEach((li) => {
-        attachEventHandlers(li);
-    });
-};
+// window.onload = function () {
+//     const lis = document.querySelectorAll("#rule-list .li");
+//     lis.forEach((li) => {
+//         attachEventHandlers(li);
+//     });
+// };
 
-async function projectRuleGeneration() {
-    const list = document.getElementById("rule-list");
-    const li = document.createElement("li");
-    li.className = "li";
+// async function projectRuleGeneration() {
+//     const list = document.getElementById("rule-list");
+//     const li = document.createElement("li");
+//     li.className = "li";
 
-    const textElement = document.createElement("span");
-    textElement.className = "text-element";
-    textElement.style.display = "none";
+//     const textElement = document.createElement("span");
+//     textElement.className = "text-element";
+//     textElement.style.display = "none";
 
-    const input = document.createElement("input");
-    input.type = "text";
+//     const input = document.createElement("input");
+//     input.type = "text";
 
-    const ruleIcon = document.createElement("span");
-    ruleIcon.className = "rule-icon";
+//     const ruleIcon = document.createElement("span");
+//     ruleIcon.className = "rule-icon";
 
-    const editIcon = document.createElement("img");
-    editIcon.src = "../../public/img/edit-right.jpg";
-    editIcon.className = "rule-img edit-icon";
+//     const editIcon = document.createElement("img");
+//     editIcon.src = "../../public/img/edit-right.jpg";
+//     editIcon.className = "rule-img edit-icon";
 
-    const deleteIcon = document.createElement("img");
-    deleteIcon.src = "../../public/img/trash.png";
-    deleteIcon.className = "rule-img delete-icon";
+//     const deleteIcon = document.createElement("img");
+//     deleteIcon.src = "../../public/img/trash.png";
+//     deleteIcon.className = "rule-img delete-icon";
 
-    ruleIcon.appendChild(editIcon);
-    ruleIcon.appendChild(deleteIcon);
+//     ruleIcon.appendChild(editIcon);
+//     ruleIcon.appendChild(deleteIcon);
 
-    li.appendChild(input);
-    li.appendChild(textElement);
-    li.appendChild(ruleIcon);
-    list.appendChild(li);
+//     li.appendChild(input);
+//     li.appendChild(textElement);
+//     li.appendChild(ruleIcon);
+//     list.appendChild(li);
 
-    attachEventHandlers(li);
-    input.focus();
+//     attachEventHandlers(li);
+//     input.focus();
 
-    // 이하에는 서버에 규칙을 추가하는 로직이 들어가야 합니다.
-    input.onkeydown = async function (event) {
-        if (event.key !== "Enter") return;
+//     // 이하에는 서버에 규칙을 추가하는 로직이 들어가야 합니다.
+//     input.onkeydown = async function (event) {
+//         if (event.key !== "Enter") return;
 
-        const rule = input.value;
+//         const rule = input.value;
 
-        let token = localStorage.getItem("token");
-        let projectId = "project ID"; // 실제 프로젝트 ID로 변경해야 합니다.
-        try {
-            const response = await axios({
-                method: "PATCH",
-                url: "/api/project/update/rule",
-                data: {
-                    rule,
-                    project_id: projectId,
-                },
-                headers: { Authorization: `Bearer ${token}` },
-            });
-            console.log(response);
-            const { success, result } = response.data;
-            if (success) {
-                console.log("규칙 추가 성공 : ", result);
-            } else {
-                console.log("규칙 추가 실패");
-            }
-        } catch (error) {
-            console.log("규칙 추가 중 에러 발생 : ", error);
-        }
-    };
-}
+//         let token = localStorage.getItem("token");
+//         let projectId = "project ID"; // 실제 프로젝트 ID로 변경해야 합니다.
+//         try {
+//             const response = await axios({
+//                 method: "PATCH",
+//                 url: "/api/project/update/rule",
+//                 data: {
+//                     rule,
+//                     project_id: projectId,
+//                 },
+//                 headers: { Authorization: `Bearer ${token}` },
+//             });
+//             console.log(response);
+//             const { success, result } = response.data;
+//             if (success) {
+//                 console.log("규칙 추가 성공 : ", result);
+//             } else {
+//                 console.log("규칙 추가 실패");
+//             }
+//         } catch (error) {
+//             console.log("규칙 추가 중 에러 발생 : ", error);
+//         }
+//     };
+// }
 
-/*파일 업로드*/
-// upload.js
-window.onload = function () {
-    document.getElementById("uploadBtn").addEventListener("click", function () {
-        document.getElementById("fileInput").click();
-    });
-};
-document.getElementById("fileInput").addEventListener("click", async function (e) {
-    const file = e.target.files[0];
-    const fileContainers = document.querySelectorAll(".api-file-contain");
+// /*파일 업로드*/
+// // upload.js
+// window.onload = function () {
+//     document.getElementById("uploadBtn").addEventListener("click", function () {
+//         document.getElementById("fileInput").click();
+//     });
+// };
+// document.getElementById("fileInput").addEventListener("click", async function (e) {
+//     const file = e.target.files[0];
+//     const fileContainers = document.querySelectorAll(".api-file-contain");
 
-    let targetDiv;
-    for (let i = 0; i < fileContainers.length; i++) {
-        if (!fileContainers[i].querySelector(".api-file-name").textContent) {
-            targetDiv = fileContainers[i].querySelector(".api-file-name");
-            break;
-        }
-    }
+//     let targetDiv;
+//     for (let i = 0; i < fileContainers.length; i++) {
+//         if (!fileContainers[i].querySelector(".api-file-name").textContent) {
+//             targetDiv = fileContainers[i].querySelector(".api-file-name");
+//             break;
+//         }
+//     }
 
-    if (targetDiv) {
-        const formData = new FormData();
-        formData.append("file", file);
-        formData.append("project_id", "your_project_id");
-        formData.append("type", "erd");
-        formData.append("project_files", "your_project_files");
+//     if (targetDiv) {
+//         const formData = new FormData();
+//         formData.append("file", file);
+//         formData.append("project_id", "your_project_id");
+//         formData.append("type", "erd");
+//         formData.append("project_files", "your_project_files");
 
-        let token = localStorage.getItem("token");
+//         let token = localStorage.getItem("token");
 
-        try {
-            const response = await axios({
-                method: "PATCH",
-                url: "/api/project/update/file",
-                data: formData,
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    "Content-Type": "multipart/form-data",
-                },
-            });
-            console.log(response);
-            const { success, result } = response.data;
-            if (result) {
-                console.log("File uploaded successfully");
-                targetDiv.textContent = name;
-            } else {
-                console.log("Failed to upload file");
-            }
-        } catch (error) {
-            console.error("Error uploading file:", error);
-        }
-    } else {
-        console.log("All divs are filled. Please add more divs.");
-    }
-});
+//         try {
+//             const response = await axios({
+//                 method: "PATCH",
+//                 url: "/api/project/update/file",
+//                 data: formData,
+//                 headers: {
+//                     Authorization: `Bearer ${token}`,
+//                     "Content-Type": "multipart/form-data",
+//                 },
+//             });
+//             console.log(response);
+//             const { success, result } = response.data;
+//             if (result) {
+//                 console.log("File uploaded successfully");
+//                 targetDiv.textContent = name;
+//             } else {
+//                 console.log("Failed to upload file");
+//             }
+//         } catch (error) {
+//             console.error("Error uploading file:", error);
+//         }
+//     } else {
+//         console.log("All divs are filled. Please add more divs.");
+//     }
+// });
