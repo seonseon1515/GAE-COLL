7cd576996e1eabf4f1ddcb6b2d9a2ffc82ac095d
Hyunkyung-Nam <gusrud13579@gmail.com>
2024-03-11 08:45:31 +0900

    유저 기본이미지 오타 수정

--- public/js/project/issue_content.js
+++ public/js/project/issue_content.js
@@ -125,7 +125,7 @@ async function addCommentFunc() {
             userImage.classList.add("user_img");
             //파일 삭제한 경우엔 액박 뜸
             if (user_img === null || user_img === "" || user_img === undefined) {
-                userImage.src = `../../../public/img/user-solid.svg;`; //
+                userImage.src = `../../../public/img/user-solid.svg`; //
             } else if (user_img.includes("http:") || user_img.includes("https://")) {
                 userImage.src = user_img;
             } else {
